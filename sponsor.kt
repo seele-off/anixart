5290ede5b5f71f93bd73deb14b254d91beaa4097
ゼーレ <131597070+seele-off@users.noreply.github.com>
2023-04-24 14:43:17 +0600

    Create sponsor.kt

---
+++ sponsor.kt
@@ -0,0 +1,34 @@
+//// ANIXART USERS ////
+anixart = users
+//// Method ////
+iget-object v0, p0, Lcom/seele/anixart/Prefs;->a:Landroid/content/SharedPreferences;
+
+    const-string v1, "IS_SPONSOR"
+
+    const/4 v2, 0x0
+
+    invoke-interface {v0, v1, v2}, Landroid/content/SharedPreferences;->getBoolean(Ljava/lang/String;Z)Z
+
+    const/4 v0, 0x1
+
+    return v0
+.end method
+//// End ////
+
+//// anixart fragment version ////
+
+[ {
+"id":"1","functional":
+  "isSponsor": "true", <!--In case of change-->
+  "is_sponsor": "trur",
+  "isVerified": "true", <!--In case of change-->
+  "is_verified": "true",
+} ]
+
+[ {
+"id":"2","XTremove":
+ "isSponsor": "false", <!--In case of change-->
+  "is_sponsor": "false",
+  "isVerified": "false", <!--In case of change-->
+  "is_verified": "false",
+ } ]
